773620754439d10482be7cae044a5a11677636f7
Ishtiak <ishtiak@genusys.us>
2025-08-04 09:53:13 +0600

    help

--- resources/views/form-feature/show.blade.php
+++ resources/views/form-feature/show.blade.php
@@ -16,12 +16,12 @@
                  data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
                  class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
                 <!--begin::Title-->
-                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">From Feature Details
+                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Help Details
                     <!--begin::Separator-->
                     <span class="h-20px border-gray-200 border-start ms-3 mx-2"></span>
                     <!--end::Separator-->
                     <!--begin::Description-->
-                    <small class="text-muted fs-7 fw-bold my-1 ms-1">Show From Feature Details</small>
+                    <small class="text-muted fs-7 fw-bold my-1 ms-1">Show Help Details</small>
                     <!--end::Description--></h1>
                 <!--end::Title-->
             </div>
@@ -47,7 +47,7 @@
                 <div class="card mt-4">
                     <div class="card-header bg-light bd-cyan">
                         <div class="card-title">
-                            <h2>From Feature Details</h2>
+                            <h2>Help Details</h2>
                         </div>
                     </div>
                     <!--begin::Body-->
