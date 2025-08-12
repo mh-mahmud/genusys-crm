a541a10a90e404317abb4a12faa3115b482c5046
Hasan <mh.mahmud.me@gmail.com>
2025-08-12 14:44:19 +0600

    update lead controller

---
+++ app/Models/RateAnalysisData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RateAnalysisData extends Model
+{
+    use HasFactory;
+
+    protected $table = "rate_analysis_data";
+}
